a5e52088e315c62db6e806ec38e09077946fec1c
devinvista <dev@invistaagora.com.br>
2026-04-19 22:54:53 -0300

    ok

---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+
+import SimpleDashboard from "@/components/simple-dashboard";
+import CompactHeader from "@/components/compact-header";
+import { useFilters } from "@/hooks/use-filters";
+
+export default function Dashboard() {
+  const { filters } = useFilters();
+  
+  return (
+    <div className="flex-1 flex flex-col overflow-hidden">
+      <CompactHeader showFilters={true} />
+      <div className="flex-1 overflow-y-auto p-6 bg-gray-50 pt-16">
+        <SimpleDashboard filters={filters} />
+      </div>
+    </div>
+  );
+}
